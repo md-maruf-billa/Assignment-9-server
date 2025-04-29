b33e99c4bb53c4ce76e2a4836a4756b2d2176e31
SadikRa <sadikrahman494@gmail.com>
2025-04-29 21:00:45 +0600

    hello sadik

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,41 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import manageResponse from './app/utils/manageRes';
-import { NextFunction } from 'express';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import cookieParser from "cookie-parser"
-import appRouter from './router';
-const app: Application = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import manageResponse from "./app/utils/manageRes";
+import { NextFunction } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
+import appRouter from "./router";
+const app: Application = express();
 
 // middlewares
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.use("/api", appRouter)
+app.use("/api", appRouter);
 
 // stating point
 
 app.get("/", (req: Request, res: Response) => {
-    manageResponse(res, {
-        success: true,
-        message: "Server is running successfully",
-        statusCode: 200,
-    })
-})
+  manageResponse(res, {
+    success: true,
+    message: "Server is running successfully",
+    statusCode: 200,
+  });
+});
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 app.use((req: Request, res: Response, nex: NextFunction) => {
-    res
-        .status(404)
-        .json({
-            success: false,
-            message: "API not found!",
-            error: {
-                path: req.originalUrl
-            }
-        })
-})
-
+  res.status(404).json({
+    success: false,
+    message: "API not found!",
+    error: {
+      path: req.originalUrl,
+    },
+  });
+});
 
 export default app;
