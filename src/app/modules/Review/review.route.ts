592e21941b1b679e8d75c1b7b5fbbd649a105277
rashidsarker <rashidrock558@gmail.com>
2025-05-01 10:41:54 +0600

    `Added auth middleware to create-review route`

--- src/app/modules/Review/review.route.ts
+++ src/app/modules/Review/review.route.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import { reviewController } from './review.controller';
 import RequestValidator from '../../middlewares/requestValidator';
 import { reviewValidation } from './review.validation';
+import auth from '../../middlewares/auth';
+import { Role } from '@prisma/client';
 
 const router = Router();
 
 router.post(
   '/create-review',
   RequestValidator(reviewValidation.createReview),
+  auth(Role.ADMIN, Role.USER),
   reviewController.createReview,
 );
 
