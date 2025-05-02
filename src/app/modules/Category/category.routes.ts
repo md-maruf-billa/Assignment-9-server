d9cfd2ed698c9b268306994faeab05e8e8367445
Sakhawat Showrav <76746821+Sakhawat71@users.noreply.github.com>
2025-05-02 12:23:52 +0600

    Sakhawat/category module (#31)
    * clean up yarn.lock
    
    * category module: add getCategories and getCategoryById methods;
    
    * implement update and delete functionality for categories
    
    * only admin can create ,update and delete Category
    
    * Remove .env from repository
    
    ---------
    
    Co-authored-by: Md Abumahid Islam <157400283+md-maruf-billa@users.noreply.github.com>

--- src/app/modules/Category/category.routes.ts
+++ src/app/modules/Category/category.routes.ts
@@ -2,13 +2,40 @@ import { Router } from 'express';
 import RequestValidator from '../../middlewares/requestValidator';
 import { categoryValidation } from './category.validation';
 import { categoryController } from './category.controller';
+import auth from '../../middlewares/auth';
+import { Role } from '@prisma/client';
 
 const router = Router();
 
+router.get(
+    '/',
+    categoryController.getCategories
+);
+
+router.get(
+    '/:id',
+    categoryController.getCategoryById
+);
+
 router.post(
-  '/create-category',
-  RequestValidator(categoryValidation.createCategory),
-  categoryController.createCategory,
+    '/create-category',
+    auth(Role.ADMIN),
+    RequestValidator(categoryValidation.createCategory),
+    categoryController.createCategory,
+);
+
+router.patch(
+    '/update/:id',
+    auth(Role.ADMIN),
+    RequestValidator(categoryValidation.updateCategory),
+    categoryController.updateCategory
+);
+
+
+router.delete(
+    '/delete/:id',
+    auth(Role.ADMIN),
+    categoryController.deleteCategory
 );
 
 export const categoryRouter = router;
--- src/app/modules/Category/category.service.ts
+++ src/app/modules/Category/category.service.ts
@@ -1,12 +1,96 @@
 import { Category } from '@prisma/client';
 import { prisma } from '../../utils/Prisma';
+import { AppError } from '../../utils/AppError';
+import httpStatus from 'http-status';
 
-const createCategory = async (reviewData: Category) => {
-  const result = await prisma.category.create({
-    data: reviewData,
-  });
-  return result;
+const createCategory = async (payload: Category) => {
+    return await prisma.category.create({
+        data: payload,
+    });
 };
+
+
+const getCategories = async () => {
+    const result = await prisma.category.findMany({
+        include: {
+            reviews: true,
+            _count: true
+        }
+    });
+    return result;
+};
+
+
+const getCategoryById = async (id: string) => {
+    const result = await prisma.category.findUnique({
+        where: {
+            id
+        },
+        include: {
+            reviews: true,
+            _count: true
+        }
+    });
+    if (!result) {
+        throw new AppError(
+            "Category Not Found",
+            httpStatus.NOT_FOUND
+        );
+    };
+    return result;
+};
+
+
+const updateCategory = async (
+    id: string,
+    payload: Category
+) => {
+    const isExist = await prisma.category.findUnique({
+        where: {
+            id
+        }
+    });
+    if (!isExist) {
+        throw new AppError(
+            "Category Not Found",
+            httpStatus.NOT_FOUND
+        );
+    };
+
+    return await prisma.category.update({
+        where: {
+            id
+        },
+        data: payload
+    });
+};
+
+
+const deleteCategory = async (id: string) => {
+    const isExist = await prisma.category.findUnique({
+        where: {
+            id
+        }
+    });
+    if (!isExist) {
+        throw new AppError(
+            "Category Not Found",
+            httpStatus.NOT_FOUND
+        );
+    };
+
+    return await prisma.category.delete({
+        where: {
+            id
+        }
+    });
+};
+
+
 export const categoryService = {
-  createCategory,
+    createCategory,
+    getCategories,
+    getCategoryById,
+    updateCategory,
+    deleteCategory
 };
