638f4013fe46bf3ae77ac0064eb5b675c5d4d420
Md Abumahid Islam <157400283+md-maruf-billa@users.noreply.github.com>
2025-05-02 18:19:29 +0600

    Development (#34)
    * Sakhawat/category module (#31)
    
    * clean up yarn.lock
    
    * category module: add getCategories and getCategoryById methods;
    
    * implement update and delete functionality for categories
    
    * only admin can create ,update and delete Category
    
    * Remove .env from repository
    
    ---------
    
    Co-authored-by: Md Abumahid Islam <157400283+md-maruf-billa@users.noreply.github.com>
    
    * Auth API Integration (#30)
    
    Co-authored-by: Md Abumahid Islam <157400283+md-maruf-billa@users.noreply.github.com>
    
    * Tesing company module
    
    ---------
    
    Co-authored-by: Sakhawat Showrav <76746821+Sakhawat71@users.noreply.github.com>
    Co-authored-by: Imam Hosen <mimam22.cse@bu.ac.bd>
    Co-authored-by: Abumahid Islam <eng.marufilla@gmail.com>

---
+++ prisma/migrations/20250502115930_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Admin" ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Company" ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "name" DROP NOT NULL;
