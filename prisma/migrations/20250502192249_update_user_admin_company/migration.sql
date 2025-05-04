7a0b6999a6517367642185fdad9378d8743a47bc
MD Rashid Sarkar <rashidrock558@gmail.com>
2025-05-04 13:14:17 +0600

    feat: implement comment routes with validation and role-based authorization and review validation updated (#41)
    * Development (#39)
    
    * added vercel.json
    
    * added vercel.json
    
    * chnage product create logic
    
    ---------
    
    Co-authored-by: Abumahid Islam <eng.marufilla@gmail.com>
    
    * feat: implement comment routes with validation and role-based authorization and review validation updated
    
    What:
    - Added routes for managing comments:
      - GET /: fetch all comments.
      - GET /:id: fetch a single comment by ID.
      - POST /create-comment: create a comment (restricted to USER and ADMIN roles) with validation.
      - PATCH /update-comment: update a comment (restricted to USER and ADMIN roles) with validation.
      - DELETE /delete-comment: soft delete a comment (restricted to USER and ADMIN roles).
    - Used `RequestValidator` middleware to validate input for creating and updating comments.
    - Integrated `auth` middleware to restrict certain actions to authorized users only.
    
    Why:
    - To allow users and admins to interact with the review comment system securely.
    - To enforce input validation and ensure only valid data is processed.
    - To protect comment operations using role-based access control and soft deletion for traceability.
    
    ---------
    
    Co-authored-by: Md Abumahid Islam <157400283+md-maruf-billa@users.noreply.github.com>
    Co-authored-by: Abumahid Islam <eng.marufilla@gmail.com>

--- prisma/migrations/20250502192249_update_user_admin_company/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "admins" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "companies" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
