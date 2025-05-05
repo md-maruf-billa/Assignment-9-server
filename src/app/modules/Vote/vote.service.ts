9a664ee8483e83eeb49dd45526334f3736be9344
rashidsarker <rashidrock558@gmail.com>
2025-05-05 11:09:01 +0600

    `Added VoteType enum and isDeleted field to Vote model, added unique constraint on reviewId and accountId`

---
+++ src/app/modules/Vote/vote.service.ts
@@ -0,0 +1,43 @@
+import { prisma } from '../../utils/Prisma';
+
+const castVote = async (
+  accountId: string,
+  reviewId: string,
+  type: 'UPVOTE' | 'DOWNVOTE',
+) => {
+  const existingVote = await prisma.vote.findUnique({
+    where: { reviewId_accountId: { reviewId, accountId } },
+  });
+
+  if (existingVote) {
+    // Update vote type if changed
+    return await prisma.vote.update({
+      where: { reviewId_accountId: { reviewId, accountId } },
+      data: { type },
+    });
+  }
+
+  // Create a new vote
+  return await prisma.vote.create({
+    data: { reviewId, accountId, type },
+  });
+};
+
+// const unvote = async (accountId: string, reviewId: string) => {
+//   const vote = await prisma.vote.findUnique({
+//     where: { reviewId_accountId: { reviewId, accountId } }
+//   });
+
+//   if (!vote) {
+//     throw new AppError('No existing vote to remove', httpStatus.NOT_FOUND);
+//   }
+
+//   return await prisma.vote.delete({
+//     where: { reviewId_accountId: { reviewId, accountId } },
+//   });
+// };
+
+export const voteService = {
+  castVote,
+  //   unvote,
+};
