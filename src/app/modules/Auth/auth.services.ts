342f184507897baef5db84d059243f2a54dc1575
Sakhawat Showrav <76746821+Sakhawat71@users.noreply.github.com>
2025-05-13 12:32:00 +0600

    Update auth.services.ts

--- src/app/modules/Auth/auth.services.ts
+++ src/app/modules/Auth/auth.services.ts
@@ -147,7 +147,7 @@ const get_my_profile_from_db = async (email: string) => {
       status: true,
       email: true,
       role: true,
-
+      isPremium: true,
       isCompleteProfile: true,
       user: true,
       admin: true,
